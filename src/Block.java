47746bf0897fd1d373f328645d8404c8faf75319
Tpool1 <tristenpool89@gmail.com>
2021-07-06 15:32:45 -0500

    add block label

--- src/Block.java
+++ src/Block.java
@@ -5,20 +5,20 @@ public class Block {
     public long timestamp;
     public String currentHash;
     public String previousHash;
-    public String data;
+    public String label;
     public int nonce;
 
-    public Block(int index, String previousHash, String data) {
+    public Block(int index, String previousHash, String label) {
         this.index = index;
         this.timestamp = System.currentTimeMillis();
-        this.data = data;
+        this.label = label;
         nonce = 0;
         currentHash = calculateHash();
     }
 
     public String calculateHash() {
         try {
-            String input = index + timestamp + previousHash + data + nonce;
+            String input = index + timestamp + previousHash + label + nonce;
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
             byte[] hash = digest.digest(input.getBytes("UTF-8"));
 
@@ -48,7 +48,7 @@ public class Block {
         String s = "Block #     : " + index + "\r\n";
          s = s + "PreviousHash     : " + previousHash + "\r\n";
          s = s + "Timestamp     : " + timestamp + "\r\n";
-         s = s + "Data          : " + data + "\r\n";
+         s = s + "Label          : " + label + "\r\n";
          s = s + "Nonce         : " + nonce + "\r\n";
          s = s + "CurrentHash       : " + currentHash + "\r\n";
     
