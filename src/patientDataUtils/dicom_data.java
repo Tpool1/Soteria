2662c359382921b20f3ab8ed2958dd70f2d15723
Tpool1 <tristenpool89@gmail.com>
2021-07-06 15:50:00 -0500

    add to dicom_data class

--- src/patientDataUtils/dicom_data.java
+++ src/patientDataUtils/dicom_data.java
@@ -1,5 +1,11 @@
 package patientDataUtils;
 
-public class dicom_data {
-    public static int patient_id;
+public class dicom_data extends image{
+    public int patient_id;
+    public image patient_image;
+
+    public void main(int patient_id, image patient_image) {
+        this.patient_id = patient_id;
+        this.patient_image = patient_image;
+    }
 }
