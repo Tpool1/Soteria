3c115424e1208c9f6fc0968ea1d592d63024683e
Tristen Pool <tristenpool89@gmail.com>
2022-06-19 12:09:44 -0500

    add batch load functionality

---
+++ src/batch_load.py
@@ -0,0 +1,15 @@
+import pydicom
+import os
+import pandas as pd
+from yaml import load
+
+from patient import patient
+from load_patient import load_patient
+
+def batch_load(clinical_path, image_dir):
+    patients = []
+    for p_dir in os.listdir(image_dir):
+        p = load_patient(clinical_path, os.path.join(image_dir, p_dir))
+        patients.append(p)
+
+    return patients
--- src/ui/ui.py
+++ src/ui/ui.py
@@ -1,6 +1,7 @@
 import PySimpleGUI as sg
 from chain import chain
 from load_patient import load_patient
+from batch_load import batch_load
 
 def ui():
 
@@ -10,7 +11,7 @@ def ui():
 
     layout = [[sg.Text("Enter a Patient Image Directory:"), sg.InputText()],
                 [sg.Text("Enter a Patient Clinical Path:"), sg.InputText()],
-                [sg.Button("OK"), sg.Button("Cancel")]]
+                [sg.Button("Single Patient"), sg.Button("Batch"), sg.Button("Cancel")]]
 
     window = sg.Window("Soteria", layout)
 
@@ -21,7 +22,7 @@ def ui():
             bc.printChain()
             break
 
-        elif event == "OK":
+        elif event == "Single Patient":
 
             p = load_patient(values[1], values[0])
 
@@ -30,5 +31,15 @@ def ui():
                 bc.makeGenesis(p)
             else:
                 bc.makeBlock("patient added via UI", p)
+
+        elif event == "Batch":
+
+            patients = batch_load(values[1], values[0])
+
+            for p in patients:
+                if len(bc.chain) == 0:
+                    bc.makeGenesis(p)
+                else:
+                    bc.makeBlock("patient added via UI in a batch", p)
 
     window.close()
